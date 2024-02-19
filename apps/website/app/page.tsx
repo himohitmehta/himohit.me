549373254e1121148fc478d71384c55ac7d35544
Mohit Mehta <mehtam2701@gmail.com>
2024-02-19 23:31:06 +0530

    added new ui components

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -2,15 +2,17 @@ import { ThemeToggle } from "@/components/ThemeToggle";
 import Hero from "@/components/home/hero";
 import Projects from "@/components/home/projects";
 import Timeline from "@/components/home/timeline";
-
+import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function Home() {
 	return (
 		<main className="px-4">
 			<ThemeToggle />
-			<Hero />
-			<Timeline />
-			<Projects />
+			<TracingBeam>
+				<Hero />
+				<Timeline />
+				<Projects />
+			</TracingBeam>
 		</main>
 	);
 }
