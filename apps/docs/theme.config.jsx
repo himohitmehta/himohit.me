8a7393b22cdea374f2d43ddcd174760a6a811012
Mohit Mehta <mehtam2701@gmail.com>
2024-03-02 14:08:07 +0530

    removed `_meta.json` files from devops folder

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,6 +1,8 @@
 export default {
 	logo: <span>himohit.me</span>,
 	project: {
-		link: "https://github.com/shuding/nextra",
+		link: "https://github.com/himohitmehta/himohit.me/tree/main/apps/docs",
 	},
+	docsRepositoryBase:
+		"https://github.com/himohitmehta/himohit.me/tree/main/apps/docs",
 };
