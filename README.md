70c45c05005058d2f83728b33237d7ac7d6554d0
Mohit Mehta <mehtam2701@gmail.com>
2024-03-02 14:42:26 +0530

    added readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# himohit.me 
+
+This is my personal portfolio website which showcases my work and experience.
+The `apps` folder consists of the applications. 
+- `website`: deployed on `https://himohit.me`
+- `blog`: deployed on `https://blog.himohit.me`
+- `docs`: deployed on `https://docs.himohit.me`
--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,8 +1,12 @@
 export default {
 	logo: <span>himohit.me</span>,
+	logoLink: "https://himohit.me",
 	project: {
 		link: "https://github.com/himohitmehta/himohit.me/tree/main/apps/docs",
 	},
 	docsRepositoryBase:
 		"https://github.com/himohitmehta/himohit.me/tree/main/apps/docs",
+	banner: {
+		text: "This website is a work in progress. I will be adding more content soon. Stay tuned!",
+	},
 };
