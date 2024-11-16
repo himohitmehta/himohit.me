19415876910502ee4af4adbc16a91213176c9375
Mohit Mehta <mehtam2701@gmail.com>
2024-11-16 14:10:59 +0530

    updated timeline font

--- apps/website/components/ui/timeline.tsx
+++ apps/website/components/ui/timeline.tsx
@@ -35,7 +35,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 	return (
 		<div className="w-full  font-sans md:px-10" ref={containerRef}>
 			<div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-				<h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
+				<h2 className="text-2xl md:text-4xl font-bold mb-4 text-black dark:text-white max-w-4xl">
 					My Journey as a developer
 				</h2>
 				<p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
